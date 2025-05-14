a4a2e07d0e6c8de2835c9bb8665aabe2158078a5
mahmoud isa <mahmoudahalim0@gmail.com>
2025-05-15 02:58:27 +0300

    fix; remove api key

--- src/services/openrouter.js
+++ src/services/openrouter.js
@@ -1,7 +1,5 @@
 import OpenAI from "openai";
-// const OPEN_ROUTER_API_KEY = import.meta.env.VITE_OPEN_ROUTER_API_KEY
-const OPEN_ROUTER_API_KEY =
-  "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";
+const OPEN_ROUTER_API_KEY = import.meta.env.VITE_OPEN_ROUTER_API_KEY
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: OPEN_ROUTER_API_KEY,
